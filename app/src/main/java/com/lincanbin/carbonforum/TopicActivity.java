b74e786b411c784548d351763d989cf5591233cd
lincanbin <lincanbin@hotmail.com>
2015-10-14 21:44:22 +0800

    TopicActivity: jump to a specific page.

--- app/src/main/java/com/lincanbin/carbonforum/TopicActivity.java
+++ app/src/main/java/com/lincanbin/carbonforum/TopicActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.lincanbin.carbonforum.adapter.PostAdapter;
 import com.lincanbin.carbonforum.config.APIAddress;
@@ -30,7 +31,8 @@ import java.util.List;
 import java.util.Map;
 
 public class TopicActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
-    private  Toolbar mToolbar;
+    private Toolbar mToolbar;
+    private TextView mTopicTitle;
     private RecyclerView mRecyclerView ;
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private FloatingActionButton mFloatingActionButton;
@@ -65,6 +67,12 @@ public class TopicActivity extends AppCompatActivity implements SwipeRefreshLayo
                 R.color.material_light_green_700
         );
         mSwipeRefreshLayout.setOnRefreshListener(this);
+        /*
+        if(Integer.parseInt(mTopicPage) == 1) {
+            mTopicTitle = (TextView) findViewById(R.id.title);
+            mTopicTitle.setText(mTopic);
+        }
+        */
         //RecyclerView
         mRecyclerView = (RecyclerView) findViewById(R.id.post_list);
         mRecyclerView.setHasFixedSize(true);
@@ -96,7 +104,7 @@ public class TopicActivity extends AppCompatActivity implements SwipeRefreshLayo
                         .setAction("Action", null).show();
             }
         });
-        loadPost(1);
+        loadPost(Integer.parseInt(mTopicPage));
     }
     //加载帖子
     private void loadPost(int targetPage) {
