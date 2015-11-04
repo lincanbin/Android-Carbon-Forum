d56ee723904927b90b5d735d6d8cb1ec490c7d6a
lincanbin <lincanbin@hotmail.com>
2015-11-04 11:13:25 +0800

    Override onOptionsItemSelected().

--- app/src/main/java/com/lincanbin/carbonforum/NewActivity.java
+++ app/src/main/java/com/lincanbin/carbonforum/NewActivity.java
@@ -76,19 +76,16 @@ public class NewActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
-
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
--- app/src/main/java/com/lincanbin/carbonforum/ReplyActivity.java
+++ app/src/main/java/com/lincanbin/carbonforum/ReplyActivity.java
@@ -1,11 +1,13 @@
 package com.lincanbin.carbonforum;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -36,6 +38,13 @@ public class ReplyActivity extends AppCompatActivity {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mContent = (EditText) findViewById(R.id.content);
         mContent.setText(defaultContent);
+        //自动弹出键盘
+        mContent.setFocusable(true);
+        mContent.setFocusableInTouchMode(true);
+        mContent.requestFocus();
+        InputMethodManager mInputManager = (InputMethodManager)mContent.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        mInputManager.showSoftInput(mContent, 0);
+
         if (mToolbar != null) {
             //mToolbar.setTitle(getString(R.string.title_activity_reply));
             if(Integer.parseInt(mPostFloor) == 0){
